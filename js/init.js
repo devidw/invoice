d421210d7f8a7115b98a1e8543a79b41d154a85f
devidwolf <68775926+devidwolf@users.noreply.github.com>
2020-12-31 17:02:51 +0100

    fix footer position

--- js/init.js
+++ js/init.js
@@ -10,7 +10,12 @@ $(function() {
     $(this).val(new Date().toISOString().slice(0, 10));
   });
   $('#dueDate').val(new Date().addDays(30).toISOString().slice(0, 10));
+  // invoice number
   $('#invoiceNumber').val(Date.now());
+
+  // footer height
+  let footerHeight = $('footer').outerHeight(true);
+  document.documentElement.style.setProperty('--footer-height', `${footerHeight}px`);
 
   let generator = new InvoiceGenerator();
   // console.log(generator);
